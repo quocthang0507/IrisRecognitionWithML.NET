84265d7db6f77ffb18e056e9fd6ad8ed372ce303
La Quốc Thắng <quocthang0507@gmail.com>
2020-08-25 21:32:03 +0700

    Update files

--- IrisRecognitionML.Model/ModelInput.cs
+++ IrisRecognitionML.Model/ModelInput.cs
@@ -4,27 +4,27 @@ using Microsoft.ML.Data;
 
 namespace IrisRecognitionML.Model
 {
-    public class ModelInput
-    {
-        [ColumnName("sepal_length"), LoadColumn(0)]
-        public float Sepal_length { get; set; }
+	public class ModelInput
+	{
+		[ColumnName("sepal_length"), LoadColumn(0)]
+		public float Sepal_length { get; set; }
 
 
-        [ColumnName("sepal_width"), LoadColumn(1)]
-        public float Sepal_width { get; set; }
+		[ColumnName("sepal_width"), LoadColumn(1)]
+		public float Sepal_width { get; set; }
 
 
-        [ColumnName("petal_length"), LoadColumn(2)]
-        public float Petal_length { get; set; }
+		[ColumnName("petal_length"), LoadColumn(2)]
+		public float Petal_length { get; set; }
 
 
-        [ColumnName("petal_width"), LoadColumn(3)]
-        public float Petal_width { get; set; }
+		[ColumnName("petal_width"), LoadColumn(3)]
+		public float Petal_width { get; set; }
 
 
-        [ColumnName("variety"), LoadColumn(4)]
-        public string Variety { get; set; }
+		[ColumnName("variety"), LoadColumn(4)]
+		public string Variety { get; set; }
 
 
-    }
+	}
 }
